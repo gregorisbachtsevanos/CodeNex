e16330cc2857fce5eb590d2a8fa16e6b9c927464
GregorisB <gregorbachts@gmail.com>
2023-11-08 23:44:37 +0200

    changes

---
+++ src/app/chat/components/Sidebar/Sidebar.styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledSidebarContainer = styled.nav`
+	width: 20%;
+`;
---
+++ src/app/chat/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledSidebarContainer } from "./Sidebar.styled";
+
+export const Sidebar = () => {
+	return <StyledSidebarContainer>Sidebar</StyledSidebarContainer>;
+};
